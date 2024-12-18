b50c85511b062e2b7f0be673b56675eb4cb22c64
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-12-18 06:48:09 +0600

    adding request Handler for practicing dry methd

--- src/modules/student/student.controller.ts
+++ src/modules/student/student.controller.ts
@@ -1,21 +1,15 @@
 import { Name } from './student.interface';
 import { z } from 'zod';
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { studentServices } from './student.service';
 import studentZvalidationSchema from './student.zod.validation';
 import { error } from 'console';
 import sendResponse from '../../utils/sendResponse';
 import HttpStatus from 'http-status';
 
-const getAllStudents = async (req: Request, res: Response, next:NextFunction) => {
+const getAllStudents:RequestHandler = async (req, res, next) => {
   try {
     const result = await studentServices.getAllStudentsFromDB();
-
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'All studends data retribe dsuccessfully',
-    //   data: result,
-    // });
 
     sendResponse(res,{
       statusCode:HttpStatus.OK,
@@ -28,17 +22,11 @@ const getAllStudents = async (req: Request, res: Response, next:NextFunction) =>
   }
 };
 
-const getSingleStudent = async (req: Request, res: Response, next:NextFunction) => {
+const getSingleStudent:RequestHandler = async (req, res, next) => {
   try {
     const { studentID } = req.params;
 
     const result = await studentServices.getSingleStudentDB(studentID);
-
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Single Student data retribed successfully',
-    //   data: result,
-    // });
     
     sendResponse(res,{
       statusCode:HttpStatus.OK,
@@ -51,17 +39,11 @@ const getSingleStudent = async (req: Request, res: Response, next:NextFunction) 
   }
 };
 
-const deleteStudent = async (req: Request, res: Response, next:NextFunction) => {
+const deleteStudent:RequestHandler = async (req, res, next) => {
   try {
     const { studentId } = req.params;
 
     const result = await studentServices.deleteStudentDB(studentId);
-
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Student is deleted successfully',
-    //   data: result,
-    // });
 
     sendResponse(res,{
       statusCode:HttpStatus.OK,
