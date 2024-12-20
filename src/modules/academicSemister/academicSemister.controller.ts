af071eeec4f7b9cf3eb2480a1cfa5ce30478c7d5
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-12-20 21:39:42 +0600

    academic semister successfull

--- src/modules/academicSemister/academicSemister.controller.ts
+++ src/modules/academicSemister/academicSemister.controller.ts
@@ -1,12 +1,13 @@
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { academicServices } from './academicSemister.service';
+import HttpStatus from 'http-status';
 
 const createAcademicSemister = catchAsync(async (req, res) => {
   const result = await academicServices.createAcademicSemisterIntoDB(req.body);
 
   sendResponse(res, {
-    statusCode: httpStatus.OK,
+    statusCode: HttpStatus.OK,
     success: true,
     messagw: 'Academic semister craeted succcessfully',
     data: result,
