492b8041cf22ff4b763f48aaacc8e1eec7912cbb
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-12-21 07:16:09 +0600

    same name year academic semister validation done

--- src/modules/academicSemister/academicSemister.model.ts
+++ src/modules/academicSemister/academicSemister.model.ts
@@ -1,19 +1,23 @@
 import { model, Schema } from 'mongoose';
 import { TAcademicSemister } from './academicSemister.interface';
 import { string } from 'zod';
-import { academicSemesterCodes, academicSemesterNames, months } from './acadicSemister.const';
+import {
+  academicSemesterCodes,
+  academicSemesterNames,
+  months,
+} from './acadicSemister.const';
 
 const academicSemisterSchema = new Schema<TAcademicSemister>(
   {
     name: {
       type: String,
       required: true,
-      enum:academicSemesterNames,
+      enum: academicSemesterNames,
     },
     code: {
       type: String,
       required: true,
-      enum:academicSemesterCodes,
+      enum: academicSemesterCodes,
     },
     year: {
       type: String,
@@ -34,6 +38,19 @@ const academicSemisterSchema = new Schema<TAcademicSemister>(
     timestamps: true,
   },
 );
+
+academicSemisterSchema.pre('save', async function (next) {
+  const isSemisterExists = await AcademicSemister.findOne({
+    year: this.year,
+    name: this.name,
+  });
+
+  if (isSemisterExists) {
+    throw new Error('Semister is already exists')
+  };
+
+  next()
+});
 
 export const AcademicSemister = model<TAcademicSemister>(
   'AcademicSemister',
