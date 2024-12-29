c61144de27ef356ca918458e6dc5167db24def10
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-12-29 07:06:58 +0600

    padStart position changed

--- src/modules/user/user.utils.ts
+++ src/modules/user/user.utils.ts
@@ -1,8 +1,8 @@
 import { TAcademicSemister } from '../academicSemister/academicSemister.interface';
 
 export const generateStudentId = (payLoad: TAcademicSemister) => {
-  const currentId = (0).toString().padStart(4, '0');
-  let incrementId = (Number(currentId) + 1).toString();
+  const currentId = (0).toString();
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payLoad.year}${payLoad.code}${incrementId}`;
 
   return incrementId;
