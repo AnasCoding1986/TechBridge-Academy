b565e301a4e54814e3d6b6454f4c15588d8b18a4
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-12-30 07:53:12 +0600

    AcademicFacultyControllers

---
+++ src/modules/academicFaculty/academicFaculty.controller.ts
@@ -0,0 +1,62 @@
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { AcademicFacultyServices } from './academicFaculty.service';
+
+const craeteAcadicFaculty = catchAsync(async (req, res) => {
+  const result = await AcademicFacultyServices.createAcademicFacultyIntoDB(
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    messagw: 'Academic Faculty craeted successfully',
+    data: result,
+  });
+});
+
+const getAllAcademicFaculties = catchAsync(async (req, res) => {
+  const result = await AcademicFacultyServices.getAllAcademicFacultiesFromDB();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    messagw: 'Academic Faculty retrived successfully',
+    data: result,
+  });
+});
+
+const getSingleAcademicFaculty = catchAsync(async (req, res) => {
+  const {facultyId} = req.params;
+  const result = await AcademicFacultyServices.getSingleAcademicFacultyFromDB(facultyId);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    messagw: 'Single Academic Faculty retrived successfully',
+    data: result,
+  });
+});
+
+const updateAcademicFaculty = catchAsync(async (req, res) => {
+  const {facultyId} = req.params;
+
+  const result = await AcademicFacultyServices.updateAcademicFacultyIntoDB(
+    facultyId,
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    messagw: 'Single Academic Faculty updated successfully',
+    data: result,
+  });
+});
+
+export const AcademicFacultyControllers = {
+    craeteAcadicFaculty,
+    getAllAcademicFaculties,
+    getSingleAcademicFaculty,
+    updateAcademicFaculty
+}
